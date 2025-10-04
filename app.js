2fb8b9b3cb87021945aa77f1e90030a2bdda326e
oluwatobicode <odetokuntreasure6@gmail.com>
2025-10-04 16:01:23 +0100

    push

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -71,14 +72,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routes
-app.get('/', (req, res) => {
-  // to pass data in our pug template create an object inside the render
-  res.status(200).render('base', {
-    tour: 'The forest hike',
-    user: 'Jonas',
-  });
-});
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ controllers/viewsController.js
@@ -0,0 +1,24 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1)  get tour data from collection
+  const tours = await Tour.find();
+  //   2) build template
+  //   3) render that template using our data from
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
